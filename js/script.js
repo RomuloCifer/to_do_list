ca1f424fdc1f99a3d1543b5c8c6e10b8928e021a
Cif <ou_iy@hotmail.com>
2026-04-11 21:33:10 -0300

    feat: implementa criação de tarefas via formulário

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-const tarefas = [];
+
+const form = document.querySelector(".task-form");
+const input = document.querySelector(".task-input");
+const list = document.querySelector(".task-list");
+
+const tasks =[];
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault(); // ñ recarregar a página
+
+    const typedText = input.value.trim();
+    if (typedText === "") {
+        return;
+    }
+
+    const newTask = {
+        id: Date.now(),
+        title: typedText,
+        completed: false,
+    }
+    tasks.push(newTask);
+    input.value = "";
+    console.log(tasks);
+})
+
